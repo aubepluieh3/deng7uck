a7bae4e49a44b15b268ecea4b7e435485fb15142
SOO <66086544+aubepluieh3@users.noreply.github.com>
2026-01-02 17:00:20 +0900

    feat: create homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import MessageForm from "../components/MessageForm";
+import MessageList from "../components/MessageList";
+import TitleSection from "../components/TitleSection";
+
+const HomePage = () => {
+    return (
+      <main className="min-h-screen bg-[#F9FAFB] px-6 py-10 space-y-14">
+        <TitleSection />
+        <MessageForm />
+        <MessageList />
+      </main>
+    );
+  };
+export default HomePage;
